e6695055b2ae3acf098fc1bfdf1c669b61e43f25
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-04-17 22:02:12 -0300

    Ex 4: Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
-import styles from './style.module.css'
-import { Link } from 'react-router-dom';
+import styles from "./style.module.css"
+import { Link } from "react-router-dom";
 
 function Header() {
   return ( 
