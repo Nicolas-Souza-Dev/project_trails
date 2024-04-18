2702d5226e55bf744f0bc548b280c10f8243a34f
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-04-17 21:59:19 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import CardTrilha from './assets/components/CardTrilha/index'
-import "./App.css"
+import "./App.css";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import {TrilhasContextProvider} from "./context/TrilhasContext"
 
 function App() {
-
-  let listaTrilhas = [
-    {
-      nomeTrilha: "Trilha da Lagoinha do Leste",
-      cidade: "Florianópolis",
-      estado: "SC",
-      duracao: 120,
-      trajeto: 4,
-      dificuldade: "Intermediário",
-      tipo: "caminhada / trekking",
-      nomeUsuario: "Nicolas Souza",
-      urlImagem: "https://images.pexels.com/photos/1270718/pexels-photo-1270718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    }
-  ]
-
   return (
-    <div className="container">
-      <h1 className="titulo">Explore trilhas incríveis</h1>
-      {
-        listaTrilhas.map((trilha, index) => (
-          <CardTrilha dadosTrilha={trilha} key={index} />
-        ))
-      }
-    </div>
-  )
+    <TrilhasContextProvider>
+      <Header />
+      <Outlet />
+    </TrilhasContextProvider>
+  );
 }
 
 export default App;
