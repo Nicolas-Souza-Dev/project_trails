599af4a3837a3271e094fcf634d9ff8d50cbbcfc
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-04-17 23:08:05 -0300

    Confirgurando Outler, Footer e Header

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+// import { Link } from "react-router-dom";
+
+function Footer() {
+ return (
+  <>
+   <div className="footer">
+    <span>Create By - Nicolas Souza</span>
+   </div>
+  </>
+ );
+}
+
+export default Footer;
