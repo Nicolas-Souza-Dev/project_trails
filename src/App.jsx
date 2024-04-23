5d96dd94cd54c5724a43da298ad7456429731b9e
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-04-23 00:04:52 -0300

    Arrumando conflito

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,6 @@ import { TrilhasContextProvider } from "./context/TrilhasContext";
 import Footer from "../src/components/Footer/footer.jsx";
 
 function App() {
-<<<<<<< HEAD
-
-  let listaTrilhas = [
-    {
-      nomeTrilha: "Trilha Lagoinha do Leste",
-      cidade: "Florianópolis",
-      estado: "SC",
-      duracao: 120,
-      trajeto: 4,
-      dificuldade: "Intermediário",
-      tipo: "caminhada / trekking",
-      nomeUsuario: "Nicolas Souza",
-      urlImagem: "https://images.pexels.com/photos/1270718/pexels-photo-1270718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    }
-  ]
-
-  return (
-    <div className="container">
-      <h1 className="titulo">Explore trilhas incríveis</h1>
-      {
-        listaTrilhas.map((trilha, index) => (
-          <CardTrilha dadosTrilha={trilha} key={index} />
-        ))
-      }
-    </div>
-  )
-=======
  return (
   <TrilhasContextProvider>
    <Header />
@@ -39,7 +12,6 @@ function App() {
    <Footer />
   </TrilhasContextProvider>
  );
->>>>>>> feature/semana-13
 }
 
 export default App;
