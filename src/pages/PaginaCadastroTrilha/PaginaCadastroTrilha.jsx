870f19423d055880486b39dc52ff3851a058495a
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-04-22 21:13:23 -0300

    Registrando campos de formulário

--- src/pages/PaginaCadastroTrilha/PaginaCadastroTrilha.jsx
+++ src/pages/PaginaCadastroTrilha/PaginaCadastroTrilha.jsx
@@ -1,6 +1,9 @@
+import { useForm } from "react-hook-form";
 import styles from "./PaginaCadastroTrilha.module.css";
 
 function PaginaCadastroTrilha() {
+ const { register } = useForm();
+
  return (
   <div className="container">
    <h1>Cadastro de nova trilha</h1>
@@ -8,31 +11,75 @@ function PaginaCadastroTrilha() {
    <form className={styles.form}>
     <div>
      <label htmlFor="nome-trilha">Nome da trilha</label>
-     <input type="text" name="" id="nome-trilha"></input>
+     <input
+      type="text"
+      {...register("name", {
+       required: "Este campo é obrigatório",
+       maxLength: {
+        value: 100,
+        message: "Este campo aceita no máximo 100 caracteres"
+       }
+      })}></input>
     </div>
     <div>
      <label htmlFor="durazao-estimada">Duração estimada (min)</label>
-     <input type="number" name="duracao-estimada" id=""></input>
+     <input
+      type="number"
+      {...register("durazao-estimada", {
+       required: "Este campo é obrigatório"
+      })}></input>
     </div>
     <div>
      <label htmlFor="trajeto">Trajeto (km)</label>
-     <input type="number" name="trajeto" id=""></input>
+     <input
+      type="number"
+      {...register("trajeto", {
+       required: "Este campo é obrigatório",
+       maxLength: {
+        value: 4,
+        message: "Esse campo aceita no maximo 4 caracteres"
+       }
+      })}></input>
     </div>
     <div>
      <label htmlFor="cidade">Cidade</label>
-     <input type="text" name="cidade" id=""></input>
+     <input
+      type="text"
+      {...register("cidade", {
+       required: "Este campo é obrigatório",
+       maxLength: {
+        value: 60,
+        message: "Este campo aceita no maximo 60 caracteres"
+       }
+      })}></input>
     </div>
     <div>
      <label htmlFor="estado">Estado</label>
-     <input type="text" name="estado" id=""></input>
+     <input
+      type="text"
+      {...register("estado", {
+       required: "Este campo é obrigatório",
+       maxLength: {
+        value: 2,
+        message: "Este campo aceita no maximo 2 caracteres"
+       }
+      })}></input>
     </div>
     <div>
      <label htmlFor="nome-usuario">Nome completo usuário</label>
-     <input type="text" name="nome-usuario" id=""></input>
+     <input
+      type="text"
+      {...register("nome-usuario", {
+       maxLength: {
+        value: 60,
+        message: "Este campo aceita no maximo 60 caracteres"
+       }
+      })}></input>
     </div>
     <div>
      <label htmlFor="dificuldade">Dificuldade</label>
      <select name="dificauldade">
+      <option value="Selecione">Selecione uma opção</option>
       <option value="Iniciante">Iniciante</option>
       <option value="intermediário">intermediário</option>
       <option value="Avançado">Avançado</option>
@@ -41,18 +88,24 @@ function PaginaCadastroTrilha() {
     <div>
      <label htmlFor="type">Tipo de trilha</label>
      <select name="type">
+      <option value="Selecione">Selecione uma opção</option>
       <option value="Caminhada">Caminhada</option>
       <option value="Trekking">Trekking</option>
      </select>
     </div>
     <div>
      <label htmlFor="url-imagem">URL imagem da trilha</label>
-     <input type="text" name="url-imagem" id=""></input>
+     <input
+      type="text"
+      {...register("url-imagem", {
+       maxLength: { value: 300 }
+      })}></input>
     </div>
    </form>
-
-   <button>Cadastrar</button>
-   <butto>Voltar</butto>
+   <div className="buttons">
+    <button>Cadastrar</button>
+    <button>Voltar</button>
+   </div>
   </div>
  );
 }
